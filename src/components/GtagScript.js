ccc639e7fe5d07b1eaadaf6040124084539852de
MrPotterOP <shubham.ubarhande1044@gmail.com>
2024-12-24 10:23:10 +0530

    mesurment id added

--- src/components/GtagScript.js
+++ src/components/GtagScript.js
@@ -18,8 +18,8 @@ export default function GtagScript() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname + window.location.search,
             });
           `,
         }}
@@ -27,3 +27,27 @@ export default function GtagScript() {
     </>
   )
 }
+// export default function GtagScript() {
+//   return (
+//     <>
+//       <Script
+//         strategy="afterInteractive"
+//         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+//       />
+//       <Script
+//         id="gtag-init"
+//         strategy="afterInteractive"
+//         dangerouslySetInnerHTML={{
+//           __html: `
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){dataLayer.push(arguments);}
+//             gtag('js', new Date());
+//             gtag('config', '${GA_TRACKING_ID}', {
+//               page_path: window.location.pathname,
+//             });
+//           `,
+//         }}
+//       />
+//     </>
+//   )
+// }
